31ed12598f59445804322796cfeb4d92aeaa4eee
udipta <iamudi03@gmail.com>
2019-03-09 01:30:34 +0530

    static template change

--- ignus.py
+++ ignus.py
@@ -7,66 +7,45 @@ from functools import wraps
 app = Flask(__name__)
 
 app.secret_key = "!@#$%^&*()a-=afs;'';312$%^&*k-[;.sda,./][p;/'=-0989#$%^&0976678v$%^&*(fdsd21234266OJ^&UOKN4odsbd#$%^&*(sadg7(*&^%32b342gd']"
-html = ['pubg','shortfilm','poster','kannada','maya',
- 'lipread','pictionary','photography','reta','gaming',
- 'oxsherlock','staroi','clickndrun','sustaineri', 'buzzwire',
- 'hotwheels',
- 'madlibs',
- 'bestoutof',
- 'bbs',
- 'arcania',
- 'terror',
- 'devil']
+
+
+html = [
+		 'gaming','staroi','lipread','pubg','photography','terror','kannada',
+		 'pictionary','shortfilm','reta','arcania', 'clickndrun',
+ 		 'bestoutof','poster','bbs','oxsherlock','madlibs','hotwheels','sustaineri','maya','devil', 'buzzwire'
+ 	]
 
 name= [
- 'GAMING',
- 'STAR OF IGNUS',
- 'LIP READ YOUR PARTNER',
- 'PUBG',
- 'PHOTOGRAPHY',
- 'TERROR HUNT',
- 'KANNADA QUIZ',
- 'PICTIONARY',
- 'SHORTFILM',
- 'RETALITION - THE SURGICAL STRIKE',
- 'ARCANIA',
- 'CLICK AND RUN',
- 'BEST OUT OF E-WASTE',
- 'POSTER PRESENTATION'
- 'BEG BORROW STEAL',
- 'OXFORD SHERLOCK',
- 'MAD LIB THEATER',
- 'HOT WHEELS',
- 'SUSTAINERI',
- 'MAYA',
- 'DEVIL SEAGUE',
- 'BUZZ FIRE',
-]
+		 'GAMING','STAR OF IGNUS','LIP READ YOUR PARTNER','PUBG', 'PHOTOGRAPHY','TERROR HUNT',
+		 'KANNADA QUIZ','PICTIONARY','SHORTFILM','RETALITION - THE SURGICAL STRIKE','ARCANIA',
+		 'CLICK AND RUN','BEST OUT OF E-WASTE','POSTER PRESENTATION','BEG BORROW STEAL', 'OXFORD SHERLOCK',
+		 'MAD LIB THEATER', 'HOT WHEELS', 'SUSTAINERI', 'MAYA', 'DEVIL SEAGUE', 'BUZZ WIRE'
+	]
 
 photo = [
-'gam.jpg',
-'sti.jpg',
-'lryp.jpg',
-'pubg.jpg',
-'pho.jpg',
-'teh.jpg',
-'kaq.jpg',
-'pict.jpg',
-'shfl.jpg',
-'retaweb.jpg',
-'arcaniaweb.jpg',
-'clickandrunweb.jpg',
-'boew.jpg',
-'popr.jpg',
-'bbs.jpg',
-'oxsh.jpg',
-'mlt.jpg',
-'hotw.jpg',
-'sust.jpg',
-'maya.jpg',
-'devs.jpg',
-'bzw.jpg',
-]
+		'gam.jpg',
+		'sti.jpg',
+		'lryp.jpg',
+		'pubg.jpg',
+		'pho.jpg',
+		'teh.jpg',
+		'kaq.jpg',
+		'pict.jpg',
+		'shfl.jpg',
+		'retaweb.jpg',
+		'arcaniaweb.jpg',
+		'clickandrunweb.jpg',
+		'boew.jpg',
+		'popr.jpg',
+		'bbs.jpg',
+		'oxsh.jpg',
+		'mlt.jpg',
+		'hotw.jpg',
+		'sus.jpg',
+		'maya.jpg',
+		'devs.jpg',
+		'bzw.jpg',
+	]
 
 @app.route("/test")
 def test():
